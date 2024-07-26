170e6ea03bb7e0a7b59a1c8e50b3891ac8868f63
josematamoros917 <josematamoros917@gmail.com>
2024-07-25 23:00:23 -0400

    navegacion compleja reparando con js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
         var target = $(this).attr('href');
         
         $('html, body').animate({
-            scrollTop: $(target).offset().top - 70 // Ajusta este valor según el tamaño de tu navbar
+            scrollTop: $(target).offset().top - 100 // Ajusta este valor según el tamaño de tu navbar
         }, 500, function () {
             setTimeout(function () {
                 bsOffcanvas.hide();
